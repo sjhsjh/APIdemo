b0aa9ce6d761cc57e8ea5a7b7b683ac932b693d7
jinhui.shao <jinhui.shao@tcl.com>
2017-08-11 19:43:59 +0800

    scrollview

--- app/src/main/java/com/example/apidemo/activity/EventDispatchActivity.java
+++ app/src/main/java/com/example/apidemo/activity/EventDispatchActivity.java
@@ -3,16 +3,23 @@ package com.example.apidemo.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
+
 import com.example.apidemo.BaseActivity;
 import com.example.apidemo.R;
 import com.example.apidemo.utils.NLog;
 
 public class EventDispatchActivity extends BaseActivity {
+    private ScrollView   mScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_dispatch);
+        mScrollView = (ScrollView) findViewById(R.id.scrollview);
+
+
 
     }
 
@@ -24,6 +31,7 @@ public class EventDispatchActivity extends BaseActivity {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        new Exception("sjh2").printStackTrace();
         NLog.i("sjh1", "activity onTouchEvent. " + event.getAction());
         return super.onTouchEvent(event);
     }
