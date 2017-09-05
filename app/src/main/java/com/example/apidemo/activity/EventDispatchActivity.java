b4acff60897d2a8b3e6fb53b4ff0157e20db4bc8
jinhui.shao <jinhui.shao@tcl.com>
2017-09-05 19:37:41 +0800

    444

--- app/src/main/java/com/example/apidemo/activity/EventDispatchActivity.java
+++ app/src/main/java/com/example/apidemo/activity/EventDispatchActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -14,10 +15,13 @@ import com.example.apidemo.view.ChildView;
 import com.example.apidemo.view.MyHorizontalScrollView;
 import java.util.ArrayList;
 import android.app.Activity;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class EventDispatchActivity extends BaseActivity {
     private MyHorizontalScrollView mScrollView;
     private ChildView mChildView;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,7 @@ public class EventDispatchActivity extends BaseActivity {
         setContentView(R.layout.event_dispatch);
         mScrollView = (MyHorizontalScrollView) findViewById(R.id.scrollview);
         mChildView = (ChildView) findViewById(R.id.childview);
+        mListView = (ListView)findViewById(R.id.listview1);
 
         WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics dm2 = new DisplayMetrics();
@@ -39,7 +44,38 @@ public class EventDispatchActivity extends BaseActivity {
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
      //   NLog.i("sjh1", "activity dispatchTouchEvent. " + event.getAction());
+        NLog.w("sjh1", "first child text : " + ((TextView)((RelativeLayout)(mListView).getChildAt(0)).getChildAt(0)).getText()
+         + "  ===  " + ((TextView)((RelativeLayout)(mListView.getAdapter().getView(10, null, null))).getChildAt(0)).getText()); // getScrollY 0
         return super.dispatchTouchEvent(event);
+    }
+
+    /**
+     * ListView通过position指定view(使用getView!)
+     */
+    public View getViewByPosition(int pos, ListView listView) {
+        final int firstPosition = listView.getFirstVisiblePosition();
+        // getChildCount（）  返回的是显示在屏幕上可见的item的数量(包含显示不玩全的)
+        final int lastPosition = firstPosition + listView.getChildCount() -1;
+        if(pos < firstPosition || pos > lastPosition) {
+            return listView.getAdapter().getView(pos, null, listView);
+        }else{
+            final int childIndex = pos - firstPosition;
+            return listView.getChildAt(childIndex);
+        }
+
+    }
+
+    /**
+     * ListView没有提供得到滚动高度的任何方法，必须自己根据getChildAt(0).top和getFirstVisiblePosition()来综合计算获得。(前提是每个item的高度相同)
+     */
+    public int getListViewScrollYDistance() {
+        View c = mListView.getChildAt(0);
+        if (c == null) {
+            return 0;
+        }
+        int firstVisiblePosition = mListView.getFirstVisiblePosition();
+        int top = c.getTop();
+        return -top + firstVisiblePosition * c.getHeight() ;
     }
 
     @Override
@@ -66,7 +102,10 @@ public class EventDispatchActivity extends BaseActivity {
 //                Toast.makeText(DemoActivity_1.this, "click item " + position, Toast.LENGTH_SHORT).show();
 //            }
 //        });
+
+
     }
+
 
 
 
--- app/src/main/java/com/example/apidemo/view/StickyLayout.java
+++ app/src/main/java/com/example/apidemo/view/StickyLayout.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Scroller;
+
 import com.example.apidemo.R;
 import com.example.apidemo.utils.NLog;
 
@@ -15,9 +18,11 @@ import com.example.apidemo.utils.NLog;
  * <br>
  * Created by jinhui.shao on 2017/8/28.
  */
-public class StickyLayout extends RelativeLayout{
+public class StickyLayout extends LinearLayout{
     private ListView mListView;
     private Button mHeadView;
+    private Scroller mScroller;
+    private int mHeadViewHeight;
 
     public StickyLayout(Context context) {
         super(context);
@@ -36,13 +41,63 @@ public class StickyLayout extends RelativeLayout{
     private void initView(){
         mListView = (ListView) findViewById(R.id.listview_slide);
         mHeadView = (Button)findViewById(R.id.head_view);   // 要clickable处理了down事件的才能拖动.
+        mScroller = new Scroller(getContext());
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        ViewGroup.LayoutParams lp = mListView.getLayoutParams();
+        mHeadViewHeight = mHeadView.getHeight();
+        lp.height = getMeasuredHeight() - mHeadViewHeight;   // 1984 - 400
     }
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
         int action = ev.getAction();
         int x = (int)ev.getRawX();
-        int y = (int)ev.getRawY();
+        int y = (int)ev.getY();
+        boolean intercept = false;
+
+        switch (action){
+            case MotionEvent.ACTION_DOWN :
+                break;
+            case MotionEvent.ACTION_MOVE :
+                NLog.d("sjh3", "dispatchTouchEvent ACTION_MOVE  y = " + y);
+//                if(mHeadView.getY() > -mHeadView.getHeight() && mHeadView.getY() <= 0
+//                        || (isListViewTop() && y - mLastInterceptY > 0)){
+//                    // 若只平移父view，注意mHeadView它一直在父view的左上角，因此此时mHeadView.getY()恒为0。
+//                    // 若listview和headview单独平移，listiew的平移会有延迟导致两者分离。分离了就不能判定listview已到顶部了。
+//                    mHeadView.setTranslationY(Math.min(0 , mHeadView.getTranslationY() + y - mLastInterceptY));
+//                    intercept = true;
+//                }
+//                else {
+//                    intercept = false;
+//                }
+                if (y - mLastInterceptY > 0 && isListViewTop()) {
+                    requestDisallowInterceptTouchEvent(false);
+                }
+                NLog.i("sjh3", "intercept = " + intercept);
+                break;
+            case MotionEvent.ACTION_CANCEL :
+            case MotionEvent.ACTION_UP :
+                break;
+
+        }
+
+//        mLastTouchX = x;
+//        mLastTouchY = y;
+//        mLastInterceptX = x;
+//        mLastInterceptY = y;
+
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        int x = (int)ev.getRawX();
+        int y = (int)ev.getY();
         boolean intercept = false;
 
         switch (action){
@@ -50,17 +105,17 @@ public class StickyLayout extends RelativeLayout{
                 break;
             case MotionEvent.ACTION_MOVE :
                 NLog.d("sjh3", "onInterceptTouchEvent ACTION_MOVE  y = " + y);
-                if(mHeadView.getY() > -mHeadView.getHeight() && mHeadView.getY() <= 0
-                        || (isListViewTop() && y - mLastInterceptY > 0)){
-                    // 若只平移父view，注意mHeadView它一直在父view的左上角，因此此时mHeadView.getY()恒为0。
-                    // 若listview和headview单独平移，listiew的平移会有延迟导致两者分离。分离了就不能判定listview已到顶部了。
-                    mHeadView.setTranslationY(Math.min(0 , mHeadView.getTranslationY() + y - mLastInterceptY));
-
+                if (getScrollY() < mHeadViewHeight || y - mLastInterceptY > 0 && isListViewTop()) {
                     intercept = true;
                 }
-                else {
-                    intercept = false;
-                }
+//                if(getY() > -mHeadView.getHeight() && getY() <= 0
+//                        || (isListViewTop() && y - mLastInterceptY > 0)){   // mHeadView.getY()恒为0,因为它一直在父view的左上角。
+//                    mHeadView.setTranslationY(Math.min(0 , mHeadView.getTranslationY() + y - mLastInterceptY));
+//                    intercept = true;
+//                }
+//                else {
+//                    intercept = false;
+//                }
 
                 NLog.i("sjh3", "intercept = " + intercept);
                 break;
@@ -73,54 +128,10 @@ public class StickyLayout extends RelativeLayout{
         mLastTouchY = y;
         mLastInterceptX = x;
         mLastInterceptY = y;
-
-        return super.dispatchTouchEvent(ev);
-    }
-
-//    @Override
-//    public boolean onInterceptTouchEvent(MotionEvent ev) {
-//        int action = ev.getAction();
-//        int x = (int)ev.getRawX();
-//        int y = (int)ev.getRawY();
-//        boolean intercept = false;
-//        switch (action){
-//            case MotionEvent.ACTION_DOWN :
-//                break;
-//            case MotionEvent.ACTION_MOVE :
-//                NLog.d("sjh3", "onInterceptTouchEvent ACTION_MOVE  y = " + y);
-//
-//                if(getY() > -mHeadView.getHeight() && getY() <= 0
-//                        || (isListViewTop() && y - mLastInterceptY > 0)){   // mHeadView.getY()恒为0,因为它一直在父view的左上角。
-//                    mHeadView.setTranslationY(Math.min(0 , mHeadView.getTranslationY() + y - mLastInterceptY));
-//                    intercept = true;
-//                }
-//                else {
-//                    intercept = false;
-//                }
-//
-//                NLog.i("sjh3", "intercept = " + intercept);
-//                break;
-//            case MotionEvent.ACTION_CANCEL :
-//            case MotionEvent.ACTION_UP :
-//                break;
-//
-//        }
-//        mLastTouchX = x;
-//        mLastTouchY = y;
-//        mLastInterceptX = x;
-//        mLastInterceptY = y;
-//        return super.onInterceptTouchEvent(ev);
-//    }
-
-    private boolean isListViewTop(){
-        boolean result = false;
-        if(mListView.getFirstVisiblePosition() == 1){
-            View view = mListView.getChildAt(0);
-//            if(view != null && view.getTop() >= 0){ // 证明该item跟listview顶部平齐.
-               result = true;
-//            }
+        if(intercept){
+            return true;
         }
-        return result;
+        return super.onInterceptTouchEvent(ev);
     }
 
     private int mLastTouchX;
@@ -128,30 +139,44 @@ public class StickyLayout extends RelativeLayout{
     private int mLastInterceptX;
     private int mLastInterceptY;
 
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) {
-//        int action = event.getAction();
-//        int x = (int)event.getRawX();
-//        int y = (int)event.getRawY();
-//        boolean intercept = false;
-//
-//        switch (action){
-//            case MotionEvent.ACTION_DOWN :
-//                break;
-//            case MotionEvent.ACTION_MOVE :
-//
-//
-//                NLog.v("sjh3", "onTouchEvent ACTION_MOVE  y = " + y);
-//                break;
-//            case MotionEvent.ACTION_CANCEL :
-//            case MotionEvent.ACTION_UP :
-//                break;
-//
-//        }
-//        mLastTouchX = x;
-//        mLastTouchY = y;
-//        return super.onTouchEvent(event);
-//    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        int x = (int)event.getRawX();
+        int y = (int)event.getY();
+        boolean result = false;
 
+        switch (action){
+            case MotionEvent.ACTION_DOWN :
+                break;
+            case MotionEvent.ACTION_MOVE :
+                scrollBy(0, -(y - mLastTouchY));
+                if (getScrollY() >= mHeadViewHeight) {
+                    event.setAction(MotionEvent.ACTION_DOWN);
+                    result = dispatchTouchEvent(event);
+                }
+
+                NLog.v("sjh3", "onTouchEvent ACTION_MOVE  y = " + y + "  == " + getScrollY());
+                break;
+            case MotionEvent.ACTION_CANCEL :
+            case MotionEvent.ACTION_UP :
+                break;
+
+        }
+        mLastTouchX = x;
+        mLastTouchY = y;
+        return result || super.onTouchEvent(event);
+    }
+
+    private boolean isListViewTop(){
+        boolean result = false;
+        if(mListView.getFirstVisiblePosition() == 0){
+            View view = mListView.getChildAt(0);
+            if(view != null && view.getTop() >= 0){ // 证明该item跟listview顶部平齐.
+                result = true;
+            }
+        }
+        return result;
+    }
 
 }
