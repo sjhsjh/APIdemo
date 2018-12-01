d703b66e9a6e109d2be82aee874a410f262db584
shaojinhui <470701083@qq.com>
2018-12-01 15:06:15 +0800

    tmp1

--- app/src/main/java/com/example/apidemo/IGSON.java
+++ app/src/main/java/com/example/apidemo/IGSON.java
@@ -1,9 +1,9 @@
 package com.example.apidemo;
 
 /**
- * Created by Administrator on 
+ * Created by Administrator on 2017/4/18
+ * tmp1
  */
 public interface IGSON {
     public void add();
-    //mster1
 }
