7a090f83de805be721dac55df81b5af5e07f1ca5
e <470701083@qq.com>
2019-02-13 23:32:52 +0800

    Guideline（辅助线）

--- app/src/main/java/com/example/apidemo/activity/GsonActivity.java
+++ app/src/main/java/com/example/apidemo/activity/GsonActivity.java
@@ -15,6 +15,7 @@ import com.google.gson.JsonSyntaxException;
 
 /**
  * Created by Administrator on 2017/4/18 0018.
+ * Gson + ConstraintLayout
  */
 public class GsonActivity extends BaseActivity {
 
