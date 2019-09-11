9cf6d790b630c016d2c1d67e9ebf32ae2afb2512
shaojinhui <shaojinhui@yy.com>
2019-09-11 10:57:31 +0800

    java工程支持kotlin运行：Tools选项，选择“Kotlin”，然后选择“Configure Kotlin in Project”

---
+++ JavaC/src/main/java/com/example/MyTest.kt
@@ -0,0 +1,31 @@
+package com.example
+
+import java.util.Random
+
+//object Mytest {
+
+//    @JvmStatic
+fun main(args: Array<String>) {
+    println("Hello,world!")
+    val r1 = Random()
+    for (i in 0..9) {
+        //  System.out.println("===" + r1.nextInt(7));
+    }
+//    //字符串转list<String>
+//    val str2 = "asdfghjkl"
+//    val lis = Arrays.asList(*str2.split("".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray())
+//    for (string in lis) {
+//        //   System.out.println(string);
+//    }
+//    //list<String>转字符串，list的内容以指定符号进行拼接
+//    println(lis.joinToString(""))
+
+
+
+
+
+
+}
+
+//}
+
