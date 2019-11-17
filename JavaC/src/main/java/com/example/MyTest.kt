8609c419b1d6d32520b6c81296ec0fc86649d7c3
e <470701083@qq.com>
2019-11-18 00:44:27 +0800

    协程

--- JavaC/src/main/java/com/example/MyTest.kt
+++ JavaC/src/main/java/com/example/MyTest.kt
@@ -1,6 +1,11 @@
 package com.example
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.lang.StringBuilder
+
 
 fun main(args: Array<String>) {
 
@@ -27,6 +32,16 @@ fun main(args: Array<String>) {
         println(it)
     }
 
+
+
+    GlobalScope.launch { // 在后台启动一个新的协程并继续
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,") // 主线程中的代码会立即执行
+    runBlocking {     // 但是这个表达式阻塞了主线程
+        delay(2000L)  // ……我们延迟 2 秒来保证 JVM 的存活
+    }
 
 
 //    demo2()
